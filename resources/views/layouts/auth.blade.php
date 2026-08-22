0cd702e03fe6230c28e67cd8bb3b38982c24df16
Christ Henry Loha <62457928+CristLoha@users.noreply.github.com>
2026-08-22 11:12:30 +0900

    UI: Add adaptive SVG favicon for light/dark themes

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -7,7 +7,7 @@
         name="viewport">
     <title>@yield('title') &mdash; SajiPOS</title>
     <!-- Favicon Sendok -->
-    <link rel="icon" type="image/svg+xml" href="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 416 512'%3E%3Cpath fill='%233949AB' d='M207.9 15.2c.8 4.7 16.1 94.5 16.1 128.8V256c0 28.3-22.9 51.2-51.2 51.2H160v176c0 15.9-12.9 28.8-28.8 28.8s-28.8-12.9-28.8-28.8V307.2h-12.8C61.3 307.2 38.4 284.3 38.4 256V144c0-34.3 15.3-124.1 16.1-128.8C55.4 7.5 62 1.6 69.8 1.6h.4C78 1.6 84.6 7.5 85.5 15.2c.8 4.7 16.1 94.5 16.1 128.8V192h32v-48c0-34.3 15.3-124.1 16.1-128.8C150.6 7.5 157.2 1.6 165 1.6h.4c7.8 0 14.4 5.9 15.3 13.6zm192 12.8c.8 4.7 16.1 94.5 16.1 128.8V483.2c0 15.9-12.9 28.8-28.8 28.8s-28.8-12.9-28.8-28.8V256c0-44.2 35.8-80 80-80v-48c0-34.3 15.3-124.1 16.1-128.8C355.4 7.5 362 1.6 369.8 1.6h.4c7.8 0 14.4 5.9 15.3 13.6z'/%3E%3C/svg%3E" />
+    <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}" />
     <!-- General CSS Files -->
     <link rel="stylesheet"
         href="{{ asset('library/bootstrap/dist/css/bootstrap.min.css') }}">
