a1f55859c354fcbab86bd74611794d8600fe2747
Christ Henry Loha <62457928+CristLoha@users.noreply.github.com>
2026-08-22 10:04:05 +0900

    Fix: Translate auth.failed message and set default locale to id

--- lang/id/auth.php
+++ lang/id/auth.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'failed' => 'Username / email atau password yang Anda masukkan salah, bro.',
+    'failed' => 'Username / email atau kata sandi yang Anda masukkan salah.',
     'password' => 'Password yang Anda masukkan salah.',
     'throttle' => 'Terlalu banyak percobaan login. Silakan coba lagi dalam :seconds detik.',
 
