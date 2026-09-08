be6dfdcac6f1daa6290c142726d094f8aeb7410c
Christ Henry Loha <62457928+CristLoha@users.noreply.github.com>
2026-09-08 21:04:48 +0900

    fix(auth): add email_verified_at to User model fillable attribute so OTP verification actually saves

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-#[Fillable(['name', 'email', 'username', 'password', 'roles', 'status_akun', 'approved_by', 'approved_at', 'rejection_reason', 'otp_code', 'otp_expires_at'])]
+#[Fillable(['name', 'email', 'username', 'password', 'roles', 'status_akun', 'approved_by', 'approved_at', 'rejection_reason', 'otp_code', 'otp_expires_at', 'email_verified_at'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
